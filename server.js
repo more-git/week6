31d9dfbbe15dbd35870f5a2833c80995026b005b
more-git <57333225+more-git@users.noreply.github.com>
2020-10-06 22:41:45 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,22 +2,26 @@ var express = require('express');
 var app = express();
 var MongoClient = require('mongodb').MongoClient;
 
-MongoClient.connect("mongodb://localhost/", function(err, db) {    
+MongoClient.connect("mongodb://localhost/", function(err, db) {
     app.post('*', function(req, res) {
-    	var jsonData = "";
-     	req.on('data', function (chunk) {
+        var jsonData = "";
+        req.on('data', function (chunk) {
                 jsonData += chunk;
-     	});
+        });
         req.on('end', function () {
             var requestObject = JSON.parse(jsonData);
-	    var myDB = db.db("week7database");    
-	    myDB.collection("todo", function(err, todo) {
-		todo.save(function (err, results){
-		    console.log(results);
-		 });
-	});    
-	res.writeHead(200);
-        res.end(JSON.stringify({}));
+            var myDB = db.db("week7database");
+            myDB.collection("todo", function(err, todo) {
+                todo.save(function (err, results){
+                    console.log(results);
+                });
+            });
+            res.writeHead(200);
+            res.end(JSON.stringify({}));
+        });
     });
-});	
-app.use(express.static('./public')) 
+    app.use(express.static('./public'))
+    app.listen(8080, function(){
+        console.log("Application is running");
+    })
+});
